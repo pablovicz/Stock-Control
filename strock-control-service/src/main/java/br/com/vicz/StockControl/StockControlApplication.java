8d2f09418aaa1a4cb717b81605d8981f450be479
pablovicz <pablowoina2205@gmail.com>
2020-08-29 17:41:54 -0300

    Commit: initial commit

---
+++ strock-control-service/src/main/java/br/com/vicz/StockControl/StockControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicz.StockControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockControlApplication.class, args);
+	}
+
+}
